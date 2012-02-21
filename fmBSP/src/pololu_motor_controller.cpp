ac003cbfa36f925d098489d332cf7e21bf6cd61d
Jeppe Pedersen <baronjeppe@gmail.com>
2012-02-21 23:00:28 +0100

    Fixed the motor direction

--- fmBSP/src/pololu_motor_controller.cpp
+++ fmBSP/src/pololu_motor_controller.cpp
@@ -6,8 +6,8 @@ PololuMotorController::PololuMotorController() {
 
 void PololuMotorController::callbackHandler(const fmMsgs::desired_speedConstPtr& msg)
 {
-	int desired_speed_left = (msg->speed_left +1)*127;
-	int desired_speed_right = (msg->speed_right +1)*127;
+	int desired_speed_left = (-msg->speed_left +1)*127;
+	int desired_speed_right = (-msg->speed_right +1)*127;
 	char pololu_data[6] = {255,0,desired_speed_left,255,1,desired_speed_right};
     std::string data;
     data.assign(pololu_data,6);
