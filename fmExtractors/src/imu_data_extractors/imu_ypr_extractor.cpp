10394cf0a0b304b88e2bc5d7dec0583340013c71
superThomz <superthomz@ubuntu.ubuntu-domain>
2012-03-22 09:30:36 +0100

    for simon

--- fmExtractors/src/imu_data_extractors/imu_ypr_extractor.cpp
+++ fmExtractors/src/imu_data_extractors/imu_ypr_extractor.cpp
@@ -5,6 +5,7 @@
 #include "fmMsgs/accelerometer.h"
 #include "fmMsgs/magnetometer.h"
 #include <cmath>
+#include "nr3.h"
 
 #define DEG2RAD M_PI/180.0
 #define RAD2DEG 180.0/M_PI
@@ -15,8 +16,8 @@ float mag[3];
 
 void accelCallback(fmMsgs::accelerometer accelMsg)
 {
-	accel[0] = accelMsg.x;
-	accel[1] = accelMsg.y;
+	accel[0] = accelMsg.y;
+	accel[1] = accelMsg.x;
 	accel[2] = accelMsg.z;
 }
 void magCallback(fmMsgs::magnetometer magMsg)
@@ -24,6 +25,29 @@ void magCallback(fmMsgs::magnetometer magMsg)
 	mag[0] = magMsg.x;
 	mag[1] = magMsg.y;
 	mag[2] = magMsg.z;
+}
+
+
+float Compass_Heading(float roll, float pitch)
+{
+  float mag_x;
+  float mag_y;
+  float cos_roll;
+  float sin_roll;
+  float cos_pitch;
+  float sin_pitch;
+  
+  cos_roll = cos(roll);
+  sin_roll = sin(roll);
+  cos_pitch = cos(pitch);
+  sin_pitch = sin(pitch);
+  
+  // Tilt compensated magnetic field X
+  mag_x = mag[0]*cos_pitch + mag[1]*sin_roll*sin_pitch + mag[2]*cos_roll*sin_pitch;
+  // Tilt compensated magnetic field Y
+  mag_y = mag[1]*cos_roll - mag[2]*sin_roll;
+  // Magnetic Heading
+  return atan2(-mag_y, mag_x);
 }
 
 int main(int argc, char **argv)
@@ -45,8 +69,8 @@ int main(int argc, char **argv)
  	 nh.param<std::string> ("subscriber_topic_mag", subscriber_topic_mag, "/fmSensors/Magnetometer"); //Specify the subscriber name for the magnetometer
 	 nh.param<int> ("update_frequency", update_frequency, 50); //Specify the update frequency
 
-	ros::Subscriber accelSub = n.subscribe(subscriber_topic_accel, 1, magCallback);
-	ros::Subscriber magSub = n.subscribe(subscriber_topic_mag, 1, accelCallback);
+	ros::Subscriber accelSub = n.subscribe(subscriber_topic_accel, 1, accelCallback);
+	ros::Subscriber magSub = n.subscribe(subscriber_topic_mag, 1, magCallback);
 	ros::Publisher	yprPub = n.advertise<fmMsgs::ypr>(publisher_topic, 1);
 
 	ros::Rate loop_rate(update_frequency);
@@ -58,34 +82,32 @@ int main(int argc, char **argv)
 	while (ros::ok())
 	{
 		 //Get pitch value from acceletometer
-		pitch = -atan2(accel[0], sqrt(accel[1]*accel[1]+accel[2]*accel[2])) - M_PI;
-		float temp = accel[1]/accel[2];
-		ROS_INFO("%f", temp);
+		pitch = -atan(accel[0]/sqrt(accel[1]*accel[1]+accel[2]*accel[2])) ;
 		 //Get roll value from accelerometer 
-		roll = atan(accel[1]/accel[2]) - M_PI;
+		roll = atan2(accel[1],accel[2]) * -1;
 		
 		/*
-		if(accel[1] > 0 && accel[0] < 0.5 && accel[0] > -0.5)
-			roll = abs(atan(accel[0]/accel[1]));		
-		else if(accel[0] > 0 && accel[1] < 0.5 && accel[1] > -0.5)
-			roll = abs((atan(accel[1]/accel[0]) + (M_PI/2)));
-		else if(accel[1] < 0 && accel[0] < 0.5 && accel[0] > -0.5)
-			roll = abs((atan(accel[0]/accel[1]) + (M_PI)));
-		else if(accel[0] < 0 && accel[1] < 0.5 && accel[1] > -0.5)
-			roll = abs((atan(accel[1]/accel[0]) + (M_PI * 1.5)));
+		 //Get pitch value from acceletometer
+		pitch = -atan2(accel[0], sqrt(accel[1]*accel[1]+accel[2]*accel[2])) ;
+		 //Get roll value from accelerometer 
+		roll = atan(accel[1]/accel[2]);
+		 //Calculate the mags
+		//mag_x = mag[0]*cos(pitch) + mag[1] * sin(roll) * sin(pitch) + mag[2] * cos(roll) * sin(pitch);
+		//mag_y = mag[1]*cos(roll) - mag[2] * sin(roll);"
+		float temp1 = (mag[2]*sin(roll)-mag[1]*cos(roll));
+		float temp2 = ( mag[0]*cos(pitch)+mag[1]*sin(pitch)*sin(roll)+mag[2]*sin(pitch)*cos(roll));
+		yaw = atan(temp1/temp2);
+		ROS_INFO("Yaw: %f, Pitch: %f, Roll: %f, X: %f, Y: %f, Z: %f, temp1 = %f, temp2 = %f", yaw * RAD2DEG, pitch, roll, mag[0], mag[1], mag[2], temp1, temp2);
+		 //Calculate yaw
+		//yaw = atan2(-mag_y, mag_x);	
 		*/
 
-		
-		 //Calculate the mags
-		mag_x = mag[0]*cos(pitch) + mag[1] * sin(roll) * sin(pitch) + mag[2] * cos(roll) * sin(pitch);
-		mag_y = mag[1]*cos(roll) - mag[2] * sin(roll);
+		yaw = atan2((mag[2]*sin(roll)-mag[1]*cos(roll)),( mag[0]*cos(pitch)+mag[1]*sin(pitch)*sin(roll)+mag[2]*sin(pitch)*cos(roll)));
+		//yaw = Compass_Heading(roll,pitch);
 
-		 //Calculate yaw
-		yaw = atan2(-mag_y, mag_x);	
-
-		ypr.pitch = pitch;
-		ypr.roll = roll;
-		ypr.yaw = yaw;			
+		ypr.pitch = pitch * RAD2DEG;
+		ypr.roll = roll* RAD2DEG;
+		ypr.yaw = yaw* RAD2DEG;			
 		
 		yprPub.publish(ypr);
 
