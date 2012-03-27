11516373a6761979438b62468da6e85501ad3900
Jeppe Pedersen <baronjeppe@gmail.com>
2012-03-27 14:24:38 +0200

    Added Node for placing the vehicle in the row the node uses a particlefilter and the LIDAR scan to place the vehicle

---
+++ fmExtractors/src/lidar_feature_extractors/lidar_inrow_vehicle_detect/inrow_vehicle_detect_node.cpp
@@ -0,0 +1,74 @@
+/****************************************************************************
+# Lidar row detect node
+# Copyright (c) 2011 Søren Hundevadt Nielsen <shn@kbm.sdu.dk>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+*****************************************************************************
+# File: AES25_node.cpp
+# Purpose: AES25 actuator driver.
+# Project: Field Robot - Vehicle Interface Computer
+# Author: Søren Hundevadt Nielsen <shn@kbm.sdu.dk>
+# Created: Jun 3, 2011 Søren Hundevadt Nielsen, Source written
+****************************************************************************/
+
+#include "ros/ros.h"
+
+#include "inrow_vehicle_detector.h"
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "row_detector");
+
+  InRowVehicleDetector rd;
+
+  ros::NodeHandle nh("~");
+  ros::NodeHandle n;
+
+  std::string lidar_sub_topic;
+  std::string viz_marker_left_pub_topic;
+  std::string viz_marker_right_pub_topic;
+  std::string row_pub_topic;
+  std::string point_cloud_pub_topic;
+  std::string point_cloud_rotated_pub_topic;
+  std::string position_sub_topic;
+
+  n.param<std::string>("lidar_sub_topic", lidar_sub_topic, "/fmSensors/lidar_scan_data");
+  n.param<std::string>("visualization_marker_left_pub_topic", viz_marker_left_pub_topic, "/fmExtractors/viz_marker_left");
+  n.param<std::string>("visualization_marker_right_pub_topic", viz_marker_right_pub_topic, "/fmExtractors/viz_marker_right");
+  n.param<std::string>("row_pub_topic", row_pub_topic, "/fmExtractors/Rows");
+  n.param<std::string>("point_cloud_pub_topic", point_cloud_pub_topic, "/fmExtractors/point_cloud");
+  n.param<std::string>("point_cloud_rotated_pub_topic", point_cloud_rotated_pub_topic, "/fmExtractors/point_cloud_rotated");
+  n.param<std::string>("position_sub_topic", position_sub_topic, "/fmExtractors/xyz_position");
+
+  rd.marker_pub = nh.advertise<visualization_msgs::MarkerArray>(viz_marker_left_pub_topic.c_str(), 1);
+
+  rd.row_pub = nh.advertise<fmMsgs::row>("/fmExtractors/rows",1);
+
+  rd.laser_scan_sub = nh.subscribe<sensor_msgs::LaserScan> (lidar_sub_topic.c_str(), 2, &InRowVehicleDetector::processLaserScan, &rd);
+  rd.position_sub = nh.subscribe<fmMsgs::Vector3> (position_sub_topic.c_str(), 2, &InRowVehicleDetector::positionCallback, &rd);
+
+  rd.point_cloud_pub = n.advertise<sensor_msgs::PointCloud>(point_cloud_pub_topic, 1);
+  rd.point_cloud_rotated_pub = n.advertise<sensor_msgs::PointCloud>(point_cloud_rotated_pub_topic, 1);
+
+  ros::spin();
+
+  return 0;
+}
---
+++ fmExtractors/src/lidar_feature_extractors/lidar_inrow_vehicle_detect/inrow_vehicle_detector.cpp
@@ -0,0 +1,97 @@
+/****************************************************************************
+ # AES25 node
+ # Copyright (c) 2012 Jeppe Pedersen <jepe009@student.sdu.dk>
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+ #
+ *****************************************************************************
+ # File: row_detection.cpp
+ # Purpose: AES25 actuator driver.
+ # Project: Field Robot - Vehicle Interface Computer
+ # Author: Søren Hundevadt Nielsen <shn@kbm.sdu.dk>
+ # Created: Jun 5, 2011 Søren Hundevadt Nielsen, Source written
+ ****************************************************************************/
+
+#include "inrow_vehicle_detector.h"
+
+InRowVehicleDetector::InRowVehicleDetector()
+{
+	particlefilter = ParticleFilter(1000,0,0,0.75,0.375,M_PI/2,0,0.1,0.1);
+
+	old_theta = 0;
+	old_x = 0;
+	old_y = 0;
+}
+
+void InRowVehicleDetector::positionCallback(const fmMsgs::Vector3::ConstPtr& position)
+{
+	x = position->x;
+	y = position->y;
+	theta = position->th;
+}
+
+void InRowVehicleDetector::processLaserScan(const sensor_msgs::LaserScan::ConstPtr& laser_scan )
+{
+	sensor_msgs::PointCloud cloud;
+
+	try
+    {
+    	projector.projectLaser(*laser_scan, cloud);
+    }
+    catch (tf::TransformException& e)
+    {
+        std::cout << "FEJL" << e.what();
+        return;
+    }
+    cloud.header.frame_id = "base_link";
+    point_cloud_pub.publish(cloud);
+
+	sensor_msgs::PointCloud cloud_filtered;
+	for (int i = 0; i < cloud.points.size(); i++)
+	{
+		if (cloud.points[i].x < 2 && cloud.points[i].x > -2 && cloud.points[i].y < 1 && cloud.points[i].y > -1)
+			cloud_filtered.points.push_back(cloud.points[i]);
+	}
+
+	fmMsgs::Vector3 v3 = particlefilter.update(cloud_filtered,dx,dy,dtheta);
+
+	sensor_msgs::PointCloud cloud_rotated;
+
+	for (int i = 0; i<cloud_filtered.points.size(); i++)
+	{
+		geometry_msgs::Point32 t;
+
+		t.x = cloud_filtered.points[i].x * cos(v3.th) - cloud_filtered.points[i].y * sin(v3.th) + v3.x;
+		t.y = cloud_filtered.points[i].x * sin(v3.th) + cloud_filtered.points[i].y * cos(v3.th) + v3.y;
+		t.z = 0;
+
+		cloud_rotated.points.push_back(t);
+	}
+
+    cloud_rotated.header.frame_id = "base_link";
+
+    point_cloud_rotated_pub.publish(cloud_rotated);
+
+    particlefilter.updateParticlesMarker();
+	visualization_msgs::MarkerArray markerArray = particlefilter.getParticlesMarker();
+
+	marker_pub.publish(markerArray);
+
+}
+
