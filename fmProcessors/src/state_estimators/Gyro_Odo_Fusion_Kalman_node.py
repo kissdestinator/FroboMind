aba5874f760da90694cff56d106b4241bc140749
Frobolab Warhorse <frobolab.corobot@gmail.com>
2012-04-24 12:44:52 +0200

    gyro odo kalman, updated, still some fail in it

--- fmProcessors/src/state_estimators/Gyro_Odo_Fusion_Kalman_node.py
+++ fmProcessors/src/state_estimators/Gyro_Odo_Fusion_Kalman_node.py
@@ -316,11 +316,11 @@ def kalman_calc(event):
 def update_Believes():
     global gyroAngVel,odoAngVel,gyroBelieve,odoBelieve
     if (gyroAngVel + odoAngVel < -0.02) or (gyroAngVel + odoAngVel > 0.02) :
-	gyroBelieve = 1
-	odoBelieve = 0
+	gyroBelieve = 0.9
+	odoBelieve = 0.1
     else:
-	gyroBelieve = 1
-        odoBelieve = 0
+	gyroBelieve = 0
+        odoBelieve = 1
 
 def drift_correction(gyroAng):
 	if (gyroAng < 0.004) and (gyroAng > -0.004):
