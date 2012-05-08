d536efdd07a54105bfad488a02463e2c7ba006b2
Jeppe Pedersen <baronjeppe@gmail.com>
2012-05-08 12:24:59 +0200

    lidar_navigator updated

--- fmExtractors/src/lidar_feature_extractors/lidar_obstacle_avoidance_and_navigator/lidar_navigator.cpp
+++ fmExtractors/src/lidar_feature_extractors/lidar_obstacle_avoidance_and_navigator/lidar_navigator.cpp
@@ -229,9 +229,9 @@ void LidarNavigator::publishVisualization(double turn_angle)
 	marker.scale.y = 0.6;
 	marker.scale.z = 0.5;
 	marker.color.a = 1.0;
-	marker.color.r = 0.0;
-	marker.color.g = 1.0;
-	marker.color.b = 0.0;
+	marker.color.r = 1.0;
+	marker.color.g = 0.0;
+	marker.color.b = 1.0;
 
 	vizMarker.markers.push_back(marker);
 
