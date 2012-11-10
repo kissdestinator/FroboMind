10e2906b8ff7f38b98cd81db7760df629740a55e
Matevz Mihalic <matevz.mihalic@gmail.com>
2012-11-10 17:39:22 +0100

    Reduce CPU usage of GTPS driver from 100% to ~0%

--- fmSensors/src/local_sensors/gtps.py
+++ fmSensors/src/local_sensors/gtps.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import asyncore, socket, time, sys
+import socket, time, sys
 import roslib; roslib.load_manifest('fmSensors')
 import rospy
 from fmMsgs.msg import gtps
 
-class GTPClient(asyncore.dispatcher):
+class GTPClient:
 
     def __init__(self):
         rospy.init_node('gtps_node')
@@ -13,31 +13,28 @@ class GTPClient(asyncore.dispatcher):
         host = rospy.get_param('~host', '127.0.0.1')
         port = rospy.get_param('~port', 15010)
         topic = rospy.get_param('~publisher_topic', 'gtps_position')
-        self.senderID = rospy.get_param('~sender_id')
+        senderID = rospy.get_param('~sender_id')
 
         print host, port, topic
 
-        asyncore.dispatcher.__init__(self)
         try:
-            self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.connect( (host, port) )
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect( (host, port) )
         except socket.error(errorno, string):
             rospy.logerr("%s: %s" % (errorno, string))
+            return
 
-        self.pub = rospy.Publisher('%s/%d' % (topic, self.senderID), gtps)
+        pub = rospy.Publisher('%s/%d' % (topic, senderID), gtps)
 
-    def handle_read(self):
-        if rospy.is_shutdown():
-            self.close()
-        else:
-            data = self.recv(8192).strip().replace(';', '')
+        while not rospy.is_shutdown():
+            data = s.recv(8192).strip().replace(';', '')
             rospy.loginfo(data)
             data = data.split(',')
 
             # Need to have at least 2 receivers
             if len(data) >= 12:
                 if data[2] == '1':
-                    if int(data[1]) == self.senderID:
+                    if int(data[1]) == senderID:
                         msg = gtps()
                         msg.time = int(data[0])
                         msg.x = int(data[3])
@@ -61,20 +58,17 @@ class GTPClient(asyncore.dispatcher):
                         msg.distances = distances
                         msg.levels = levels
 
-                        self.pub.publish(msg)
+                        pub.publish(msg)
                 else:
                     rospy.logwarn("Received data was not valid.")
             else:
                 rospy.logerr("Received data was in wrong format!")
 
-    def handle_error(self):
-        t, e, trace = sys.exc_info()
-        rospy.logerr("%s: %s" % (t, e))
+        s.close()
 
 if __name__ == '__main__':
     try:
         GTPClient()
-        asyncore.loop()
     except rospy.ROSInterruptException: pass
 
 
