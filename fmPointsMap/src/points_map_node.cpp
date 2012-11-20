aff9a9c31b75add3ad4ceb3b9f9085ef0900fb03
viki <viki@KISS.(none)>
2012-11-20 06:37:38 -0600

    Adding the source file for the fmPointsMap

---
+++ fmPointsMap/src/points_map_node.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "../../fmControllers/src/motor_controller/motor_controller.h"
+#include "fmMsgs/motor_power.h"
+#define _FREQUENCE_ 500
+#define _MAX_MESSAGES_ 10
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "points_map");
+	ros::NodeHandle nh;
+
+	/* Declaration of the topic to publish */
+	ros::Publisher motor_power_pub = nh.advertise<fmMsgs::motor_power>("motor_power", _MAX_MESSAGES_);
+	fmMsgs::motor_power motor_power_msg;
+
+	/* Motor msg filling (without header): */
+	motor_power_msg.power_right = 1;
+	motor_power_msg.power_left = 1;
+	
+
+	ros::Rate loop_rate(_FREQUENCE_);
+
+	while (ros::ok())
+	{
+		motor_power_pub.publish(motor_power_msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+  return 0;
+}
