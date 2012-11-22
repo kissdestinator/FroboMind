adc4b383f96408abd65afd32c9b96c6f4b227f4d
Auzias <viki@KISS.(none)>
2012-11-22 08:13:21 -0600

    fmPoint publishes AND subscribe. Need to create a service node wich subscribe to the fmMsgs/gtps_position/10522 and send the last position when call.

--- fmPointsMap/src/points_map_node.cpp
+++ fmPointsMap/src/points_map_node.cpp
@@ -2,31 +2,40 @@
 #include "navigation/Navigation.h"
 #include "../../fmControllers/src/motor_controller/motor_controller.h"
 #include "fmMsgs/motor_power.h"
+#include "fmMsgs/gtps.h"
 #define _FREQUENCE_ 500
-#define _MAX_MESSAGES_ 10
+#define _MAX_MESSAGES_ 1
 
+void printPosition(const fmMsgs::gtpsConstPtr& msg)
+{
+//  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  cout << "x: " << msg->x  << ", y: " << msg->y << endl;
+}
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "points_map");
-	ros::NodeHandle nh;
+  ros::init(argc, argv, "points_map");
+  ros::NodeHandle nh;
 
-	/* Declaration of the topic to publish */
-	ros::Publisher motor_power_pub = nh.advertise<fmMsgs::motor_power>("/fmControllers/motor_power", _MAX_MESSAGES_);
-	fmMsgs::motor_power motor_power_msg;
+  /* Declaration of the topic to publish */
+  ros::Publisher motor_power_pub = nh.advertise<fmMsgs::motor_power>("/fmControllers/motor_power", _MAX_MESSAGES_);
+  fmMsgs::motor_power motor_power_msg;
+  /* Declaration of the topic to publish */
+  ros::Subscriber sub = nh.subscribe("fmMsgs/gtps_position/10522", _MAX_MESSAGES_, printPosition);
 
-	/* Motor msg filling (without header): */
-	motor_power_msg.power_right = 1;
-	motor_power_msg.power_left = 1;
+  /* Motor msg filling (without header): */
+  motor_power_msg.power_right = 0.2;
+  motor_power_msg.power_left = 0.2;
+/*
+  ros::Rate loop_rate(_FREQUENCE_);
 
-
-	ros::Rate loop_rate(_FREQUENCE_);
-
-	while (ros::ok())
-	{
-		motor_power_pub.publish(motor_power_msg);
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+  while (ros::ok())
+  {
+    motor_power_pub.publish(motor_power_msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+*/
+  ros::spin();
   return 0;
 }
