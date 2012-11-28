5fbeec220ebcfa9eb00242dabec7f8441d6e1bfb
destinator <destinator@destiNix.(none)>
2012-11-28 16:09:03 +0100

    Ca marche. -sorry I'm french, I don't speak english, so I cannot notify you that the GTPS service works.... :(-

--- fmSensors/src/odometry_feedback/gtps_service.cpp
+++ fmSensors/src/odometry_feedback/gtps_service.cpp
@@ -4,24 +4,19 @@
 #include "GTPS_response.h"	//Response class which implements
 
 #define _MAX_MESSAGES_ 1
-#define _GTPS_TOPIC_ "fmMsgs/gtps_position/10522"
+#define _GTPS_TOPIC_ "/fmSensors/gtps_position/10522"
 #define _GTPS_SRV_ "gtps_service"
 
 using namespace std;
-
-void doit(const fmMsgs::gtps::ConstPtr& msg)
-{
-ROS_INFO("GTPS_response.h says toto!");
-}
 
 int main(int argc, char **argv)
 {
   /* initialize ros usage */
   ros::init(argc, argv, "gtps_service");
   ros::NodeHandle nh;
-ROS_INFO("GTPS_response.h says Hi!");
+
   GTPS_response service;
-  ros::Subscriber sub = nh.subscribe(_GTPS_TOPIC_, _MAX_MESSAGES_, doit);//&GTPS_response::update, &service);
+  ros::Subscriber sub = nh.subscribe(_GTPS_TOPIC_, _MAX_MESSAGES_, &GTPS_response::update, &service);
   ros::ServiceServer ss = nh.advertiseService(_GTPS_SRV_, &GTPS_response::response, &service);
 
   ros::spin();
