d6b4b8b85a03aa998b7b1b83555cada9d728b69d
Constantina <con@Con.(none)>
2012-11-29 09:45:18 +0100

    initialization updated. - missing angle calculation

--- fmPointsMap/src/navigation/Navigation.h
+++ fmPointsMap/src/navigation/Navigation.h
@@ -67,7 +67,7 @@ public:
   void set_destinations(int destinations) {_destination = destinations;}
   void set_client(ros::NodeHandle n) { _client = n.serviceClient<fmSensors::GTPS>(_GTPS_SRV_); }
   //! Set the first angle, the orientation of the robot.
-  void set_orientation(double angle){_current_angle = angle;}
+  //void set_orientation(double angle){_current_angle = angle;}
   //!Initiate the Navigation class, calculting the angle...
   void initiation();
   //!Update the current angle, will make the robot move!
