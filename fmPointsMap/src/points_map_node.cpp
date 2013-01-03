9e49ef6d28fc54b8681eae12aaf76d32d13f4827
Auzias <auzias.mael+_github@gmail.com>
2013-01-03 12:39:09 +0100

    Let's test the face destination feature

--- fmPointsMap/src/points_map_node.cpp
+++ fmPointsMap/src/points_map_node.cpp
@@ -3,6 +3,7 @@
 
 #define _MOTOR_TOPIC_ "/fmControllers/motor_power"
 #define _GTPS_TOPIC_  "/fmSensors/gtps_position/10522"
+#define _WEB_TOPIC_  "/web/destination"
 #define _MAX_MESSAGES_ 1
 
 int main(int argc, char **argv)
@@ -18,9 +19,13 @@ int main(int argc, char **argv)
   
   /* Declaration of the navigation system */
   Navigation nav(nh, map);
-  ros::Subscriber sub = nh.subscribe(_GTPS_TOPIC_,
+  ros::Subscriber sub_position = nh.subscribe(_GTPS_TOPIC_,
 				      _MAX_MESSAGES_,
 				      &Navigation::update,
+				      &nav);
+  ros::Subscriber sub_destination = nh.subscribe(_WEB_TOPIC_,
+				      _MAX_MESSAGES_,
+				      &Navigation::set_new_destination,
 				      &nav);
   nav.start();
   //ros::spin();
